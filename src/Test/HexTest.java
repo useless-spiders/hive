8ceaceb9079bfdaa061f147a2bcee9e3a40a02b4
NielsTRS <trsniels@gmail.com>
2024-04-30 14:07:16 +0200

    Ajout test

--- src/Test/HexTest.java
+++ src/Test/HexTest.java
@@ -27,7 +27,6 @@ public class HexTest {
     @Test
     public void testCell() {
         grid.setCell(0, 0, cell);
-        System.out.println(grid.getCell(0, 0));
         assertEquals(cell, grid.getCell(0, 0));
     }
 
@@ -41,4 +40,27 @@ public class HexTest {
         HexCell retrievedCell = grid.getCell(0, 0);
         assertNull(retrievedCell);
     }
+
+    @Test
+    public void testGetAdj() {
+        HexCell cell1 = new HexCell(HexCell.TYPE_ANT);
+        HexCell cell2 = new HexCell(HexCell.TYPE_BEETLE);
+        HexCell cell3 = new HexCell(HexCell.TYPE_BEE);
+        HexCell cell4 = new HexCell(HexCell.TYPE_GRASSHOPPER);
+        HexCell cell5 = new HexCell(HexCell.TYPE_SPIDER);
+        HexCell cell6 = new HexCell(HexCell.TYPE_ANT);
+        grid.setCell(0, 0, cell1);
+        grid.setCell(1, 0, cell2);
+        grid.setCell(1, 1, cell3);
+        grid.setCell(0, 1, cell4);
+        grid.setCell(-1, 0, cell5);
+        grid.setCell(-1, -1, cell6);
+        assertEquals(cell1, grid.getAdj(0, 0, ""));
+        assertEquals(cell2, grid.getAdj(0, 0, ""));
+        assertEquals(cell3, grid.getAdj(0, 0, ""));
+        assertEquals(cell4, grid.getAdj(0, 0, ""));
+        assertEquals(cell5, grid.getAdj(0, 0, ""));
+        assertEquals(cell6, grid.getAdj(0, 0, ""));
+    }
+    
 }
