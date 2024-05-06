cc64e086ec2ee887beea6eba3383369b45a57193
NielsTRS <trsniels@gmail.com>
2024-05-07 00:57:15 +0200

    Correction déplacement Ant

--- src/Modele/Insect/Ant.java
+++ src/Modele/Insect/Ant.java
@@ -4,6 +4,7 @@ import Modele.Player;
 import Structures.HexCoordinate;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import Modele.HexGrid;
 
@@ -21,20 +22,32 @@ public class Ant extends Insect {
     }
 
     @Override
-    public ArrayList<HexCoordinate> getPossibleMovesCells(int x, int y, HexGrid g) { // A faire
+    public ArrayList<HexCoordinate> getPossibleMovesCells(int x, int y, HexGrid g) {
         ArrayList<HexCoordinate> coordinates = new ArrayList<>();
+        HashSet<HexCoordinate> visited = new HashSet<>();
         if (canMoveInsect(g, this.getPlayer())) {
-            String[] directions = {"NO", "NE", "E", "SE", "SO", "O"};
-            int[] dx = {0, 1, 1, 0, -1, -1};
-            int[] dy = {-1, -1, 0, 1, 1, 0};
-
-            for (int i = 0; i < directions.length; i++) {
-                if (g.getAdj(x, y, directions[i]) == null) {
-                    coordinates.add(new HexCoordinate(x + dx[i], y + dy[i]));
-                }
-            }
+            getPossibleMovesCellsHelper(x, y, g, coordinates, new HexCoordinate(x, y), visited);
         }
         return coordinates;
+    }
+
+    private void getPossibleMovesCellsHelper(int x, int y, HexGrid g, ArrayList<HexCoordinate> coordinates, HexCoordinate original, HashSet<HexCoordinate> visited) {
+        HexCoordinate current = new HexCoordinate(x, y);
+        if (!current.equals(original)) {
+            coordinates.add(current);
+        }
+
+        String[] directions = {"NO", "NE", "E", "SE", "SO", "O"};
+        int[] dx = {0, 1, 1, 0, -1, -1};
+        int[] dy = {-1, -1, 0, 1, 1, 0};
+
+        for (int i = 0; i < directions.length; i++) {
+            HexCoordinate next = new HexCoordinate(x + dx[i], y + dy[i]);
+            if (!visited.contains(next) && g.getAdj(x, y, directions[i]) == null && g.isHiveConnectedAfterMove(original, next)) {
+                visited.add(next);
+                getPossibleMovesCellsHelper(next.getX(), next.getY(), g, coordinates, original, visited);
+            }
+        }
     }
 
     @Override
