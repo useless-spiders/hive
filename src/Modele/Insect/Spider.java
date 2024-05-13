64b915f0859c3692869b9f0a077cd71d42181347
corentin <cocoelabricot@gmail.Com>
2024-05-13 13:37:49 +0200

    fix spider v2

--- src/Modele/Insect/Spider.java
+++ src/Modele/Insect/Spider.java
@@ -43,13 +43,11 @@ public class Spider extends Insect {
             String[] directions = {"NO", "NE", "E", "SE", "SO", "O"};
             int[] dx = {0, 1, 1, 0, -1, -1};
             int[] dy = {-1, -1, 0, 1, 1, 0};
-            Log.addMessage("case de base : "+x+" "+y);
             
 
             for (int i = 0; i < directions.length; i++) {
                 HexCoordinate next = new HexCoordinate(x + dx[i], y + dy[i]);
                 if (!visited.contains(next) && g.getAdj(x, y, directions[i]) == null && g.isHiveConnectedAfterMove(original, next)) {
-                    Log.addMessage("case test : "+next.getX()+" "+next.getY());
                     //il faut qu il y ait un cote non vide pour glisser et un cote non vide sinon on va dans un trou
                     String dir = directions[((((i - 1) % directions.length) + directions.length) % directions.length)];
                     HexCell adj = g.getAdj(x, y, dir);
@@ -57,15 +55,12 @@ public class Spider extends Insect {
 
                     if(original.getX() == x + dx[((i-1)+dx.length)%dx.length] & original.getY() == y + dy[((i-1)+dy.length)%dy.length]){
                         adj = null;
-                        Log.addMessage("cas test 1" );
                     }
                     if(original.getX() == x + dx[(i+1)%dx.length] & original.getY() == y + dy[(i+1)%dy.length]){
                         adj2 = null;
-                        Log.addMessage("cas test 2");
                     }
-                    
+
                     if ((adj == null && adj2 != null) || (adj != null && adj2 == null)) {
-                        Log.addMessage("case retenue : "+next.getX()+" "+next.getY());
                         visited.add(next);
                         getPossibleMovesCellsHelper(next.getX(), next.getY(), g, steps - 1, coordinates, original, visited);
                     }
