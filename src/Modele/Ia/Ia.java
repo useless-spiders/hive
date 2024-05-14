dc9e26230755f40350fa570e40b01d4169c72d48
GabrielBettinger <bettingg38@gmail.com>
2024-05-14 11:13:40 +0200

    debut ia

--- src/Modele/Ia/Ia.java
+++ src/Modele/Ia/Ia.java
@@ -1,26 +1,51 @@
 package Modele.Ia;
 
+import java.util.ArrayList;
+
 import Controleur.Game;
 import Modele.HexGrid;
+import Modele.Move;
+import Modele.Player;
 import Structures.HexCoordinate;
 import Structures.Log;
 
 public abstract class Ia
 {
-    
+    Player us;
     HexGrid Grid;
+	ArrayList<HexCoordinate> pieces;
 
-    public static Ia nouvelle(Game g, String ia) 
+    public static Ia nouvelle(Game g, String ia, Player p) 
 	{
 		Ia resultat = null;
 		switch (ia) {
 			case "Aleatoire":
-				resultat = new IaAleatoire(g);
+				resultat = new IaAleatoire(g, p);
 				break;
 			default:
 				Log.addMessage("IA de type " + ia + " non supportée");
 		}
 		return resultat;
+	}
+
+	void initPieces()
+	{
+		Move init = new Move(null, null, null);
+		if(this.us.getColor() == "WHITE")
+		{
+			pieces = init.getWhitePieces();
+		}
+		else 
+		{
+			if(this.us.getColor() == "BLACK")
+			{
+				pieces = init.getBlackPieces();
+			}
+			else
+			{
+				Log.addMessage("couleur non blache ni noir");
+			}
+		}
 	}
 
 	void joue() 
--- src/Modele/Ia/IaAleatoire.java
+++ src/Modele/Ia/IaAleatoire.java
@@ -1,16 +1,55 @@
 package Modele.Ia;
 
+import java.awt.Desktop.Action;
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Set;
 
 import Controleur.Game;
+import Modele.Player;
+import Modele.Insect.Insect;
+import Modele.Move;
+import Structures.HexCoordinate;
 
 public class IaAleatoire extends Ia
 {
     
     Random r;
-    public IaAleatoire(Game g)
+    public IaAleatoire(Game g, Player p)
     {
         this.Grid = g.getGrid();
         this.r = new Random();
+        this.us = p;
+        initPieces();
     }
+
+    @Override
+    void joue() 
+	{
+        if(this.us.getTurn() <= 1)
+        {
+            if(this.Grid.getGrid().isEmpty())
+            {
+                
+            }
+        }
+        else
+        {
+            ArrayList<Move> possibleActions = new ArrayList<>();
+            ArrayList<HexCoordinate> possibleMoves = new ArrayList<>();
+            Set<HexCoordinate> coordinates = this.Grid.getGrid().keySet();
+            for(HexCoordinate h : coordinates)
+            {
+                Insect ins = this.Grid.getCell(h).getTopInsect();
+                if(ins.getPlayer()==this.us)
+                {
+                    possibleMoves = ins.getPossibleMovesCells(h.getX(), h.getY(), this.Grid);
+                    for(HexCoordinate x : possibleMoves)
+                    {
+                        possibleActions.add(new Move(ins, h, x));
+                    }
+                }
+            }
+        }
+	}
 }
