17f7649fe21effc3bd41c47d1374bcc4eef7b679
GabrielBettinger <bettingg38@gmail.com>
2024-05-21 14:49:44 +0200

    ajout IA2

---
+++ src/Model/Ai/Ai2.java
@@ -0,0 +1,169 @@
+package Model.Ai;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import Model.HexCell;
+import Model.HexGrid;
+import Model.Move;
+import Model.Player;
+import Model.Tree;
+import Model.Node;
+import Model.Insect.Ant;
+import Model.Insect.Bee;
+import Model.Insect.Beetle;
+import Model.Insect.Grasshopper;
+import Model.Insect.Insect;
+import Model.Insect.Spider;
+import Pattern.GameActionHandler;
+import Structure.HexCoordinate;
+import Structure.Log;
+
+public class Ai2 extends Ai
+{
+
+    Player other;
+    private Tree config;
+
+    public Ai2(GameActionHandler gameActionHandler, Player p) {
+        this.gameActionHandler = gameActionHandler;
+        this.aiPlayer = p;
+        if (this.gameActionHandler.getPlayer1() == aiPlayer) {
+            this.other = this.gameActionHandler.getPlayer2();
+        } else {
+            this.other = this.gameActionHandler.getPlayer1();
+        }
+    }
+
+
+    int heuristique(HexGrid g) {
+        int result = 0;
+        for (HexCoordinate h : g.getGrid().keySet()) {
+            HexCell cell = g.getCell(h);
+            Insect insect = cell.getTopInsect();
+
+            if (insect instanceof Ant && insect.getPlayer() == this.aiPlayer) {
+                result += 3;
+            }
+            if (insect instanceof Ant && insect.getPlayer() != this.aiPlayer) {
+                result -= 3;
+            }
+
+            if (insect instanceof Beetle && insect.getPlayer() == this.aiPlayer) {
+                result += 2;
+            }
+            if (insect instanceof Beetle && insect.getPlayer() != this.aiPlayer) {
+                result -= 2;
+            }
+
+            if (insect instanceof Grasshopper && insect.getPlayer() == this.aiPlayer) {
+                result += 2;
+            }
+            if (insect instanceof Grasshopper && insect.getPlayer() != this.aiPlayer) {
+                result -= 2;
+            }
+
+            if (insect instanceof Spider && insect.getPlayer() == this.aiPlayer) {
+                result += 1;
+            }
+            if (insect instanceof Spider && insect.getPlayer() != this.aiPlayer) {
+                result -= 1;
+            }
+            if (insect instanceof Bee) {
+                if (insect.getPlayer() == this.aiPlayer) {
+                    result -= g.getNeighborsCoordinates(h).size() * 20;
+                } else {
+                    result += g.getNeighborsCoordinates(h).size() * 20;
+                }
+            }
+        }
+        return result;
+    }
+
+    int maxTree(Node n, HexGrid g, int level)
+    {
+        Log.addMessage(level + "!");
+        if(level >= 2)
+        {
+            Player us_c = this.aiPlayer.clone();
+            HexGrid gridC = g.clone();
+            gridC.applyMove(n.getMove(), us_c);
+            int heuristique = heuristique(gridC);
+            n.setValue(heuristique);
+            return heuristique;
+        }
+        else
+        {
+            int max = -9999;
+            for (Move m : getMoves(this.aiPlayer)) 
+            {
+                Node nextMove = new Node(m);
+                n.newChild(nextMove);
+                Player us_c = this.aiPlayer.clone();
+                HexGrid gridC = g.clone();
+                gridC.applyMove(m, us_c);
+                int currentH = minTree(nextMove, gridC, level++);
+                if(currentH  > max)
+                {
+                    max = currentH;
+                }
+            }
+            n.setValue(max);
+            return max;
+        }
+
+    }
+
+    int minTree(Node n, HexGrid g, int level)
+    {
+        Log.addMessage(level + "!");
+        if(level >= 2)
+        {
+            Player us_c = this.aiPlayer.clone();
+            HexGrid gridC = g.clone();
+            gridC.applyMove(n.getMove(), us_c);
+            int heuristique = heuristique(gridC);
+            n.setValue(heuristique);
+            return heuristique;
+        }
+        else
+        {
+            int min = 9999;
+            for (Move m : getMoves(this.aiPlayer)) 
+            {
+                Node nextMove = new Node(m);
+                n.newChild(nextMove);
+                Player us_c = this.aiPlayer.clone();
+                HexGrid gridC = g.clone();
+                gridC.applyMove(m, us_c);
+                int currentH;
+                if((currentH = maxTree(nextMove, gridC, level++)) < min)
+                {
+                    min = currentH;
+                }
+            }
+            n.setValue(min);
+            return min;
+        }
+
+    }
+
+
+    public Move chooseMove() 
+    {
+        this.config = new Tree();
+        maxTree(this.config.getCurrent(),this.gameActionHandler.getGrid(), 0);
+        int max = -9999;
+        Move returnMove = null;
+        for(Node child : this.config.getCurrent().getChilds())
+        {
+            if(child.getValue() > max)
+            {
+                max = child.getValue();
+                returnMove = child.getMove();
+            }
+        }
+        return returnMove;
+    }
+    
+}
---
+++ src/Model/Node.java
@@ -0,0 +1,68 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Node 
+{
+    Node parent;
+    ArrayList<Node> childs;
+
+    Move move;
+
+    int value;
+
+    public Node(Move m)
+    {
+        this.move = m;
+        this.childs = new ArrayList<>();
+    }
+
+    // constructeur racine
+    public Node()
+    {
+        this.move = null;
+        this.childs = new ArrayList<>();
+    }
+
+    public Move getMove()
+    {
+        return this.move;
+    }
+
+    public void setMove(Move m)
+    {
+        this.move = m;
+    }
+
+    public int getValue()
+    {
+        return this.value;
+    }
+
+    public void setValue(int value)
+    {
+        this.value = value;
+    }
+
+    public void newChild(Node n)
+    {
+        n.parent = this;
+        this.childs.add(n);
+    }
+
+    public Node getParent()
+    {
+        return this.parent;
+    }
+
+    public ArrayList<Node> getChilds()
+    {
+        return this.childs;
+    }
+
+
+    public boolean isLeaf()
+    {
+        return this.childs.isEmpty();
+    }
+}
