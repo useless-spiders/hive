9240eb96e5bb92f73feac13ffa4c6cf17dbf92eb
NielsTRS <trsniels@gmail.com>
2024-05-27 16:11:48 +0200

    Commentaires

--- src/Structure/HexCoordinate.java
+++ src/Structure/HexCoordinate.java
@@ -3,23 +3,43 @@ package Structure;
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * Classe pour les coordonnées hexagonales
+ */
 public class HexCoordinate implements Cloneable, Serializable {
     private int x;
     private int y;
 
+    /**
+     * Constructeur
+     * @param x Coordonnée x
+     * @param y Coordonnée y
+     */
     public HexCoordinate(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Renvoie la coordonnée x
+     * @return int
+     */
     public int getX() {
         return this.x;
     }
 
+    /**
+     * Renvoie la coordonnée y
+     * @return int
+     */
     public int getY() {
         return this.y;
     }
 
+    /**
+     * Test si les coordonnées sont égales
+     * @return boolean
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -32,11 +52,19 @@ public class HexCoordinate implements Cloneable, Serializable {
         return this.x == other.x && this.y == other.y;
     }
 
+    /**
+     * Renvoie le hashcode
+     * @return int
+     */
     @Override
     public int hashCode() {
         return Objects.hash(this.x, this.y);
     }
 
+    /**
+     * Clone l'objet
+     * @return HexCoordinate
+     */
     @Override
     public HexCoordinate clone() {
         try {
--- src/Structure/ViewMetrics.java
+++ src/Structure/ViewMetrics.java
@@ -1,22 +1,41 @@
 package Structure;
 
+/**
+ * Classe pour les métriques de la vue
+ */
 public class ViewMetrics {
     public static int viewOffsetX;
     public static int viewOffsetY;
 
+    /**
+     * Met à jour la position de la vue
+     * @param dx Déplacement en x
+     * @param dy Déplacement en y
+     */
     public static void updateViewPosition(int dx, int dy) {
         viewOffsetX += dx;
         viewOffsetY += dy;
     }
 
+    /**
+     * Renvoie le décalage en x
+     * @return int
+     */
     public static int getViewOffsetX() {
         return viewOffsetX;
     }
 
+    /**
+     * Renvoie le décalage en y
+     * @return int
+     */
     public static int getViewOffsetY() {
         return viewOffsetY;
     }
 
+    /**
+     * Reset la position de la vue
+     */
     public static void resetViewPosition() {
         viewOffsetX = 0;
         viewOffsetY = 0;
